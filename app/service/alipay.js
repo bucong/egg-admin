ca64c4836ba5f3c8b61a79ce8e95e2bd1033e944
bucong <531053266@qq.com>
2019-08-24 21:37:37 +0800

    init

--- app/service/alipay.js
+++ app/service/alipay.js
@@ -5,14 +5,22 @@ const Service = require('egg').Service;
 const Alipay = require('alipay-mobile');
 
 class AlipayService extends Service {
+  // 创建支付订单，返回支付地址
   async doAlipay(orderData) {
     return new Promise((resolve, reject) => {
+      // 实例化 alipay
       const service = new Alipay(this.config.alipayOptions);
       service.createPageOrderURL(orderData, this.config.alipayBasicParams)
       .then(res => {
         resolve(res.data)
       })
     })
+  }
+  // 验证异步通知的数据是否正确
+  alipayNotify(params){
+    // 实例化 alipay
+    const service = new Alipay(this.config.alipayOptions);
+    return service.makeNotifyResponse(params);
   }
 }
 
